932b36878315543bbe7d7617d4969957d0cd3f45
WillRazorFace <willrazorface90s@gmail.com>
2020-09-06 17:26:54 -0300

    Armazenamento de feedback

--- inicio/forms.py
+++ inicio/forms.py
@@ -4,10 +4,10 @@ from crispy_forms.helper import FormHelper
 from email_validator import validate_email
 
 
-class FeedbackForm(forms.ModelForm):
+class FeedbackFormNaoLogado(forms.ModelForm):
     # Form para o envio de feedback
     def __init__(self, *args, **kwargs):
-        super(FeedbackForm, self).__init__(*args, **kwargs)
+        super(FeedbackFormNaoLogado, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_show_labels = False
 
@@ -26,3 +26,21 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model = models.Feedback
         fields = ('email', 'feedback')
+
+
+class FeedbackFormLogado(forms.ModelForm):
+    # Form para o envio de feedback
+    def __init__(self, *args, **kwargs):
+        super(FeedbackFormLogado, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
+
+    feedback = forms.CharField(
+        required=True,
+        widget=forms.Textarea(attrs={'placeholder': 'Deixe-nos saber sua opinião', 'rows': '3', 'cols': '10'}),
+        max_length=5000,
+    )
+
+    class Meta:
+        model = models.Feedback
+        fields = ('feedback',)
--- inicio/views.py
+++ inicio/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-from .forms import FeedbackForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import FeedbackFormNaoLogado, FeedbackFormLogado
 
 def index(request):
     if request.method == 'GET':
-        form = FeedbackForm()
-        
         if request.user.is_authenticated:
-            form.fields['email'].initial = request.user.email
-            form.fields['email'].widget.attrs['disabled'] = True
+            form = FeedbackFormLogado()
+        else:
+            form = FeedbackFormNaoLogado()
 
         return render(request, 'inicio/index.html', {'form': form})
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            form = FeedbackFormLogado(data=request.POST)
+
+            feedback = form.save(commit=False)
+            feedback.email = request.user.email
+            feedback.usuario = request.user
+            feedback.save()
+
+            messages.success(request, 'Agradecemos seu feedback.')
+            return redirect('index')
+        else:
+            form = FeedbackFormNaoLogado(data=request.POST)
+
+            form.save()
+
+            messages.success(request, 'Agradecemos seu feedback.')
+            return redirect('index')
