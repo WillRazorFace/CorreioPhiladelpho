9ba7b22395c4c3d5c609c2103b5ce11e716c08c4
WillRazorFace <willrazorface90s@gmail.com>
2020-09-09 09:52:23 -0300

    Models Categoria, Post e Comentario

--- inicio/admin.py
+++ inicio/admin.py
@@ -6,4 +6,24 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'email', 'feedback', 'data')
     list_display_links = ('data', 'feedback')
 
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    list_display_links = ('nome',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'subtitulo', 'categoria', 'data')
+    list_display_links = ('titulo',)
+
+
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'post', 'data')
+    list_display_links = ('data',)
+
+
+
 admin.site.register(models.Feedback, FeedbackAdmin)
+admin.site.register(models.Categoria, CategoriaAdmin)
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Comentario, ComentarioAdmin)
--- inicio/models.py
+++ inicio/models.py
@@ -1,6 +1,56 @@
 from django.db import models
 from usuarios.models import Usuario
 from django.utils import timezone
+from ckeditor.fields import RichTextField
+from utils.imagens import redimensionar
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+
+    def __str__(self) -> str:
+        return self.nome
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+
+class Post(models.Model):
+    usuario = models.ForeignKey(to=Usuario, on_delete=models.SET_NULL, null=True, verbose_name='Usuário')
+    titulo = models.CharField(max_length=100, verbose_name='Título')
+    subtitulo = models.CharField(max_length=300, verbose_name='Sub-título')
+    conteudo = RichTextField(verbose_name='Conteúdo')
+    foto = models.ImageField(upload_to='posts/%Y/%m/%d', blank=True, null=True, verbose_name='Imagem')
+    data = models.DateTimeField(default=timezone.now, verbose_name='Data de Postagem')
+    categoria = models.ForeignKey(to=Categoria, on_delete=models.SET_NULL, null=True, verbose_name='Categoria')
+
+    def __str__(self) -> str:
+        return self.titulo
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.foto:
+            redimensionar(self.foto.name, 1000)
+
+    class Meta:
+        verbose_name = 'Publicação'
+        verbose_name_plural = 'Publicações'
+
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(to=Usuario, on_delete=models.SET_NULL, null=True, verbose_name='Usuário')
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, verbose_name='Postagem')
+    conteudo = RichTextField(verbose_name='Conteúdo')
+    data = models.DateTimeField(default=timezone.now, verbose_name='Data do comentário')
+
+    def __str__(self) -> str:
+        return f'{self.usuario} - {self.post} - {self.data}'
+
+    class Meta:
+        verbose_name = 'Comentário'
+        verbose_name_plural = 'Comentários'
 
 
 class Feedback(models.Model):
