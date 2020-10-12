e3d7a28517a82e7684cc3cf9c4f8d55532adf656
WillRazorFace <willrazorface90s@gmail.com>
2020-10-12 16:58:59 -0300

    Adicionados scripts para o input de imagem

--- static/js/parciais/autenticacao.js
+++ static/js/parciais/autenticacao.js
@@ -1,3 +1,21 @@
 if (localStorage.getItem("modoEscuro")){
     document.body.classList.add('modo-escuro');
 }
+
+// Input customizado de imagem no form de registro
+
+const input_imagem = document.getElementById("id_foto");
+const botao_customizado = document.getElementById("foto-perfil-input");
+const texto_customizado = document.getElementById("foto-perfil-texto");
+
+botao_customizado.addEventListener("click", function() {
+    input_imagem.click();
+});
+
+input_imagem.addEventListener("change", function() {
+    if(input_imagem.value){
+        texto_customizado.innerHTML = input_imagem.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+    } else {
+        texto_customizado.innerHTML = "Nenhuma imagem selecionada";
+    }
+})
