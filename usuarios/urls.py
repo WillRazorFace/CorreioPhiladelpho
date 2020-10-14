917ab0ca498dd3b1528ce1456ccea1c6311ae786
WillRazorFace <willrazorface90s@gmail.com>
2020-10-14 00:21:07 -0300

    Urls para views de validação do form de registro adicionadas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('entrar', views.entrar, name='entrar'),
     path('registrar', views.registrar, name='registrar'),
+    path('registrar/validar-nome', views.validacao_nome_registro, name='validar-nome'),
+    path('registrar/validar-sobrenome', views.validacao_sobrenome_registro, name='validar-sobrenome'),
+    path('registrar/validar-email', views.validacao_email_registro, name='validar-email'),
+    path('registrar/validar-senha', views.validacao_senha_registro, name='validar-senha'),
     path('sair', views.sair, name='sair'),
 ]
