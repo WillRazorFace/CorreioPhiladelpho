f775f9bc8fe53f4d56f2d992a7926a879df30548
WillRazorFace <willrazorface90s@gmail.com>
2020-10-19 14:14:17 -0300

    Correção das views na nova app "sobre"

--- sobre/views.py
+++ sobre/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
-
+from inicio.forms import FeedbackFormLogado, FeedbackFormNaoLogado
 
 @require_GET
 def termos(request):
-    return render(request, 'sobre/termos-e-condicoes.html')
+    if request.user.is_authenticated:
+        form = FeedbackFormLogado()
+    else:
+        form = FeedbackFormNaoLogado()
+    
+    return render(request, 'sobre/termos.html', {'form': form})
 
 
 @require_GET
 def equipe(request):
-    return render(request, 'sobre/equipe.html')
+    if request.user.is_authenticated:
+        form = FeedbackFormLogado()
+    else:
+        form = FeedbackFormNaoLogado()
+    
+    return render(request, 'sobre/equipe.html', {'form': form})
