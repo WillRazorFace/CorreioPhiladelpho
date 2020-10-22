8c4d112e0728ac1e76d07880ab0d3a15427692a8
WillRazorFace <willrazorface90s@gmail.com>
2020-10-22 11:51:58 -0300

    Barra de leitura corrigida

--- static/js/parciais/barra-de-leitura.js
+++ static/js/parciais/barra-de-leitura.js
@@ -7,6 +7,14 @@ function barraDeProgresso(){
 
     const progresso_de_leitura = Math.min(Math.ceil(scroll / fim_da_publicacao * 100), 100);
     barra_de_progresso.style.width = `${progresso_de_leitura}%`;
+    
+    const main = document.getElementById("main");
+
+    if(barra_de_progresso.style.width == "100%"){
+        barra_de_progresso.style.display = "none";
+    } else {
+        barra_de_progresso.style.display = "block";
+    }
 }
 
 window.onload = function(){
