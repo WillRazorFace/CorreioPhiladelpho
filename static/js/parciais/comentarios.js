11f61d1e2c67e6c0caf5e42ad3e422a10df55b49
WillRazorFace <willrazorface90s@gmail.com>
2020-10-21 22:28:38 -0300

    Tratamento de erros nos scripts de comentários

--- static/js/parciais/comentarios.js
+++ static/js/parciais/comentarios.js
@@ -1,18 +1,27 @@
 function abrirFormulario(id){
-    var comentario = document.getElementById(id);
+    var comentario = document.getElementById("mostrar-" + id);
 
     if(document.contains(document.getElementById("resposta"))){
         document.getElementById("resposta").remove();
     }
 
+    if(!comentario){
+        comentario = document.getElementById("responder-" + id);
+    }
+
     comentario.insertAdjacentHTML(
         "afterend",
-        '<form method="POST" id="resposta" action="" id="form-comentario"> \
+        '<form method="POST" id="resposta" action="" id="form-comentario" style="position: relative"> \
             <textarea name="conteudo" cols="40" rows="5" maxlength="5000" placeholder="Digite para responder" required="" id="id_conteudo"></textarea> \
             \
+            <button class="fechar-formulario-comentario" onclick="fecharFormulario()">x</button> \
             <button type="submit" class="botao-comentar">Responder</button> \
         </form>'
     )
+}
+
+function fecharFormulario(){
+    document.getElementById("resposta").remove();
 }
 
 function abrir(id){
