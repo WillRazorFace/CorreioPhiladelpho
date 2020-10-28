64d4336c69233ab519974f26c95aa1bcec2367b2
WillRazorFace <willrazorface90s@gmail.com>
2020-10-28 16:17:34 -0300

    Validação de nomes corrigida

--- utils/validadores.py
+++ utils/validadores.py
@@ -2,7 +2,7 @@ from re import compile, search
 
 def checar_caracteres_especiais_e_numeros(string: str) -> bool:
     # Checa se uma string possui caracteres especiais ou números
-    checagem = compile('[^a-zA-Z]+')
+    checagem = compile('[^a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+')
 
     if checagem.search(string):
         return True
