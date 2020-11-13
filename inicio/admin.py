d05cd988ae6d35b58ed05cb0582b4f817d2e490d
WillRazorFace <willrazorface90s@gmail.com>
2020-11-13 14:37:35 -0300

    Funcionalidade de pesquisa nos templates de administrador

--- inicio/admin.py
+++ inicio/admin.py
@@ -6,25 +6,29 @@ from . import models
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'email', 'feedback', 'data')
     list_display_links = ('data', 'feedback')
+    search_fields = ('usuario__nome', 'email', 'feedback')
 
 
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('nome',)
     list_display_links = ('nome',)
+    search_fields = ('nome',)
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'subtitulo', 'categoria', 'data')
     list_display_links = ('titulo',)
+    search_fields = ('titulo', 'subtitulo', 'categoria__nome')
 
 
-class ComentarioAdmin(admin.ModelAdmin):
+class ComentarioAdmin(MPTTModelAdmin):
     list_display = ('usuario', 'post', 'conteudo', 'data')
     list_display_links = ('conteudo',)
+    search_fields = ('conteudo', 'usuario__nome', 'usuario__email')
 
 
 
 admin.site.register(models.Feedback, FeedbackAdmin)
 admin.site.register(models.Categoria, CategoriaAdmin)
 admin.site.register(models.Post, PostAdmin)
-admin.site.register(models.Comentario, MPTTModelAdmin)
+admin.site.register(models.Comentario, ComentarioAdmin)
--- professores/admin.py
+++ professores/admin.py
@@ -5,6 +5,6 @@ from . import models
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('usuario',)
     list_display_links = ('usuario',)
-
+    search_fields = ('usuario__nome',)
 
 admin.site.register(models.Professor, ProfessorAdmin)
